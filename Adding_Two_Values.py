16cdcd4b3f8b9abfab27f84516e9d513c1ea27af
PrasadMallampati <107248009+prasadmallampati@users.noreply.github.com>
2024-06-27 17:32:01 +0530

    Add files via upload

---
+++ Adding_Two_Values.py
@@ -0,0 +1,14 @@
+# adding Two values
+a=int(input('Enter a value='))# reading a value dynamically
+b=int(input('Enter b value='))# reading b value dynamically
+print(f'a+b={a+b}') # logic for addition of numbers
+
+
+'''
+output as follows
+
+Enter a value=10
+Enter b value=2
+a+b=12
+
+'''
